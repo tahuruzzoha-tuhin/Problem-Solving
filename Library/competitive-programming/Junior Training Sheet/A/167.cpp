deaf1be5816e7ecb2f594eedb5c912e5a84a40d9
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2022-11-19 20:22:40 +0600

    Re-arranging All

--- Library/competitive-programming/Junior Training Sheet/A/167.cpp
+++ Library/competitive-programming/Junior Training Sheet/A/167.cpp
@@ -1,13 +1,7 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
 
     int n, m;
     cin >> n >> m;
