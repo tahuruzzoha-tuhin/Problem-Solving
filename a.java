3dd8d16f0ab6ae40f4bf56e6b816c7d1c5a363cd
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2022-12-18 09:42:11 +0600

    [UPSOLVED] --> all

---
+++ a.java
@@ -0,0 +1,18 @@
+/****************************************************************\
+                   BISMILLAHIR RAHMANIR RAHIM
+****************************************************************
+               AUTHOR NAME: MD. TAHURUZZOHA TUHIN
+\****************************************************************/
+
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+
+public class a {
+    public static void main(String[] args){
+        double t = 1.4732;
+        Scanner input = new Scanner(System.in);
+        t = input.nextDouble();
+        System.out.println(t);
+    }
+}
