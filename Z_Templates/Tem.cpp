3d9001165fc02b0b0cfd54535b340eac41910bf5
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2023-05-29 14:59:14 +0600

    Modified

--- Z_Templates/Tem.cpp
+++ Z_Templates/Tem.cpp
@@ -1,115 +1,253 @@
 /****************************************************************\
                    BISMILLAHIR RAHMANIR RAHIM
-****************************************************************
-               AUTHOR NAME: MD. TAHURUZZOHA TUHIN
 \****************************************************************/
 
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+ 
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iterator>
+  
+#include <set>
+#include <map>
+#include <list>
+#include <stack>
+#include <tuple>
+#include <queue>
+#include <deque>
+#include <vector>
 
-#define lli             long long int
-#define F               first
-#define S               second
-#define PB              push_back
-#define si              set <lli>
-#define vi              vector <lli>
-#define pii             pair <lli, lli>
-#define vpii            vector <pii>
-#define vpp             vector <pair <lli, pii>>
-#define mii             map <lli, lli>
-#define mpi             map <pii, lli>
-#define spi             set <pii>
-#define list            list <lli>
-#define endl            "\n"
-#define vsz(x)          ((lli) x.size())
-#define all(p)          p.begin(), p.end()
-#define rall(p)         p.rbegin(), p.rend()
-#define sorta(v)        sort(all(v))
-#define sortd(v)        sort(rall(v))
-#define double          long double
-#define TLE cerr<<"Time Elapsed "<<(double)clock()/CLOCKS_PER_SEC <<" s"<<endl;
+#include <utility>
+#include <bitset>
+#include <limits.h>
+ 
 
-lli const MAX_CAP = 1e9;
-lli const MOD = 1e9 + 7;
-lli const MAX_RNG = 1024;
-lli globalArr[MAX_RNG];
+using std::get;
+using std::sort;
+using std::stoi;
+
+using std::find;
+using std::copy;
+using std::move;
+using std::swap;
+using std::fixed;
+using std::getline;
+
+using std::make_pair;
+using std::to_string;
+using std::upper_bound;
+using std::lower_bound;
+using std::setprecision;
+ 
+using std::cin;
+using std::cout;
+using std::cerr;
+
+using std::set;
+using std::map;
+using std::list;
+using std::tuple;
+using std::stack;
+using std::queue;
+using std::deque;
+using std::string;
+using std::vector;
+
+using std::pair;
+using std::less;
+using std::bitset;
+using std::greater;
+using std::unordered_map;
+using std::priority_queue;
+using std::istream_iterator;
+using std::ostream_iterator;
+ 
+ 
+typedef long double                         ld;
+typedef unsigned                            ui;
+typedef long long                           ll;
+typedef unsigned long long                  ull;
+
+typedef vector<int>                         vi;
+typedef vector<vi>                          vvi;
+typedef vector<ld>                          vld;
+typedef vector<ui>                          vui;
+typedef vector<ll>                          vll;
+typedef vector<vll>                         vvll;
+typedef vector<ull>                         vull;
+typedef vector<vull>                        vvull;
+
+typedef list<int>                           lsi;
+typedef list<ld>                            lsld;
+typedef list<ui>                            lsui;
+typedef list<ll>                            lsll;
+typedef list<ull>                           lsull;
+
+typedef set<int>                            si;
+typedef set<ld>                             sld;
+typedef set<ui>                             sui;
+typedef set<ll>                             sll;
+typedef set<ull>                            sull;
+ 
+typedef pair<int, int>                      pii;
+typedef pair<ll, ll>                        pll;
+typedef pair<ull, ull>                      pull;
+typedef pair<ld, ld>                        pld;
+
+typedef vector <pii>                        vpii;
+typedef vector <pair <ll, pii>>             vpp;
+typedef map<int, int>                       mii;
+typedef map<pii, int>                       mpii;
+typedef map<ll, ll>                         mll;
+typedef map<pll, ll>                        mpll;
+
+typedef unordered_map<int, int>             umii;
+typedef unordered_map<ll, ll>               umll;
+typedef unordered_map<char, int>            umci;
+typedef unordered_map<char, ll>             umcl;
+
+
+#define endl                                "\n"
+#define vsz(x)                              ((ll) x.size())
+#define all(p)                              p.begin(), p.end()
+#define rall(p)                             p.rbegin(), p.rend()
+#define sorta(v)                            sort(all(v))
+#define sortd(v)                            sort(rall(v))
+ 
+#define f0(i,n)                             for(int i = 0; i <  (n); i++)
+#define f1(i,n)                             for(int i = 1; i <= (n); i++)
+#define rep0(i,l,r)                         for(int i=(l); i <  (r); i++)
+#define rep1(i,l,r)                         for(int i=(l); i <= (r); i++)
+ 
+#define len(a)                              ((int)(a).size())
+#define lcm(a, b)                           (((a)*(b))/__gcd(a,b))
+#define mem(a, b)                           memset(a, b, sizeof(a))
+ 
+#define pb                                  push_back
+#define pf                                  push_front
+#define mp                                  make_pair
+#define ff                                  first
+#define ss                                  second
+ 
+#define yes                                 cout << "yes" << endl
+#define no                                  cout << "no" << endl
+#define Yes                                 cout << "Yes" << endl
+#define No                                  cout << "No" << endl
+#define YES                                 cout << "YES" << endl
+#define NO                                  cout << "NO" << endl
+#define clean                               fflush(stdout)
+
+ 
+#define Inf                                 (int)(1e9)
+#define INF                                 (ll)(1e18)
+#define Eps                                 (ld)(1e-9)
+#define EPS                                 (ld)(1e-18)
+#define PI                                  (ld)(3.141592653589793238462643383279502884197169L)
+#define MOD                                 (int)(1e9+7)
+#define MXN                                 (int)(1e5+7)
+#define TLE                                 cerr<<"Time Elapsed "<<(double)clock()/CLOCKS_PER_SEC <<" s"<<endl;
+
+
+inline ll min(ll a, ll b) {
+    return (a < b ? a : b);
+}
+ 
+inline ll max(ll a, ll b) {
+    return (a > b ? a : b);
+}
 
 void file()
 {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r+", stdin);
-    freopen("output.txt", "w+", stdout);
+    #ifdef ONLINE_JUDGE
+        freopen("input.txt", "r+", stdin);
+        freopen("output.txt", "w+", stdout);
     #endif
+
+   
 }
 
 void config()
 {
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
     cout << setprecision(15) << fixed;
+
+    #ifdef LOCAL
+        using namespace std::chrono;
+        auto begin = steady_clock::now();
+        cout << "\nTime : " 
+                << (ld)duration_cast<nanoseconds>
+                (end - begin).count()/1000000000 
+                << "s" << endl;
+    #endif
+
     // file();
 
 }
-bool binary_search(lli arr[], lli n, lli key)
-{
-    lli left = 0, right = n-1, mid;
-    while(left <= right)
-    {
-        lli mid = (left+right)/2;
-        if(arr[mid] == key) return true;
-        else if(arr[mid] > key) right = mid - 1;
-        else left = mid + 1;
-    }
-    return false;
-}
-
-bool isPrime(lli n)
-{
-    if (n <= 1) return false;for (lli i = 2; i < n; i++) 
-    if (n % i == 0) return false; return true;
-}
 
 string read_string() {
-    string s; unsigned int uc; int c;
-    while ((uc = (unsigned int)getchar()) <= 32u);
+    string s; ui uc; ui c;
+    while ((uc = (ui)getchar()) <= 32u);
     if (uc < 256u) s.push_back((char)uc);
     while ((c = getchar()) > 32) s.push_back((char)c);
     return s;
 }
 
-void Accepted();
+bool isPrime(ll n)
+{
+    if (n <= 1) return false;for (ll i = 2; i < n; i++) 
+    if (n % i == 0) return false; return true;
+}
 
+umll check_freq_int(vector<ll> x) {
+    umll freq;
+    for (auto num : x) {
+        freq[num]++;
+    }
+    return freq;
+}
+
+umcl check_freq_string(string x) {
+    umcl freq;
+    for (auto c : x) {
+        freq[c]++;
+    }
+    return freq;
+}
+void business_logic();
 
 int32_t main()
 {
-    config();
-    lli test_kase = 1;
-    scanf("%lld", &test_kase);
-    while(test_kase--) Accepted();
+    config();    
+    int Test_case  = 1;
+    cin >> Test_case;
+    for(int Case=1; Case<=Test_case; Case++){
+        // cout << "Case " << Case << ": " << business_logic() << endl;
+        business_logic();
+    }
+    // while(Test_kase--) business_logic();
     // TLE;
     return 0;
 }
 
-void Accepted()
+void business_logic()
 {
-    lli n=0, m=0, p=0, q=0, r=0;
-    lli a=0, b=0, c=0, d=0, e=0, f=0;
-    lli ans=0, cnt=0, zero=0, one=0;
-    lli first=0, second=0, last=0, middl=0;
-    lli i=0, j=0, k=0, l=0, sum=0, len=0;
-    string St, Sp="";
-
-    scanf("%lld", &n); 
-    scanf("%lld %lld", &n, &m);
-    St = read_string();
-
-    vi arr(n);
-    for(lli i=0; i<n; i++) {
-        scanf("%lld", &arr[i]);
-    }
+    ll n=0, m=0, x=0, y=0;
+    string st, sp="";
     
-    for(lli i=0; i<n; i++) {
-        printf("%lld ", arr[i]);
-    }
+    // cin >> n >> m;
+    // St = read_string();
+    // vll vec {istream_iterator<ll>(cin), istream_iterator<ll>()};
+    // copy(vec.begin(), vec.end(), ostream_iterator<ll>(cout, " "));
+    
+    cin >> n;
+    vll vec1(n); f0(i,n) cin >> vec1[i];
+    vll vec2(n); f0(i,n) cout << vec1[i] << " ";
+
+
+
     
 }
--- Z_Templates/Tem.rs
+++ Z_Templates/Tem.rs
@@ -5,29 +5,58 @@
 \****************************************************************/
 
 
-#[allow(dead_code)]
-fn scan<T: std::str::FromStr>() -> T
+#![allow(unused)]
+#[warn(dead_code)]
+
+use std::io;
+use std::cmp;
+use std::str::FromStr;
+use std::collections::HashSet;
+
+type LL = i64;
+type UL = u64;
+type US = usize;
+
+
+fn input<T: FromStr>() -> T
 {
     static mut BUFFER: Vec<String> = vec![];
     loop {
-        if let Some(token) = unsafe { BUFFER.pop() } {
-            return token.parse().ok().unwrap();
+        if let Some(props) = unsafe { BUFFER.pop() } {
+            return props.parse().ok().unwrap();
         }
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).ok();
+        io::stdin().read_line(&mut input).ok();
         unsafe { BUFFER = input.split_whitespace().rev().map(String::from).collect(); }
     }
 }
 
 fn main()
 {
-    for _ in 0..scan() {
-        let (m, mut s): (_, i64) = (scan(), scan());
-        let a = (0..m).map(|_| scan()).collect::<Vec<i64>>();
-        let mx = *a.iter().max().unwrap();
-        s -= (1..=mx).sum::<i64>() - a.iter().sum::<i64>();
-        for i in mx+1.. {
-            
-        }
+    let test: LL = input();
+    // let test: LL = 1;
+    for _ in 0..test{
+        accepted();
     }
+}
+
+fn accepted(){
+
+    let n = input();
+    // let (p, q): (LL, LL) = (input(), input());
+    // let a = (0..m).map(|_| input()).collect::<Vec<LL>>();
+
+    // let (n, k):(usize,LL) = (input(), input());
+    // let mut a: Vec<LL> = vec![0; n + 1];
+    // let mut b: Vec<LL> = vec![0; n + 1];
+
+    let mut vector = Vec::new(); //push
+    let mut my_set = HashSet::new(); //insert
+
+    for i in 0..n{
+        let value:LL = input();
+        vector.push(value);
+    }
+
+    println!("{:?}", vector);
 }
